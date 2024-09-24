68e61ecb2655f4f34ea0d17c75127439a94bb675
MartinSibul <162053837+MartinSibul@users.noreply.github.com>
2024-09-24 12:37:31 +0300

    Add files via upload

---
+++ Kell ja Discord Sõnum.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+from discordwebhook import Discord
+
+
+
+
+i = 0
+while i == 0:
+    current_time = datetime.datetime.now()
+    DATE = datetime.date.today().day
+    HOUR = datetime.datetime.now().hour   # the current hour
+    MINUTE = datetime.datetime.now().minute # the current minute
+    
+    print(str(HOUR) + ":" + str(MINUTE))
+    time.sleep(1)
+    if MINUTE == 0:
+        discord = Discord(url="https://discord.com/api/webhooks/1285532569252663329/Exwjdv428I8ev5W3gXDqowPnWX7c2cybHOpn0sGB4RowZha_XyIF-FzqvbkQtpaQ4XLx")
+        discord.post(content="balls")
+    
+
+    
+
+
+
+
+
+
+
+
+
