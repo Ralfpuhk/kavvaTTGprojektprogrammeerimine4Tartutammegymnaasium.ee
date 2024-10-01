cf0c5a7625c58d85ab50c2fb9c6af6050ec302b2
MartinSibul <162053837+MartinSibul@users.noreply.github.com>
2024-10-01 11:57:13 +0300

    Update Kell ja Discord Sõnum.py

--- Kell ja Discord Sõnum.py
+++ Kell ja Discord Sõnum.py
@@ -2,21 +2,26 @@ import time
 import datetime
 from discordwebhook import Discord
 
+# discord boti webhook: "https://discord.com/api/webhooks/1285532569252663329/Exwjdv428I8ev5W3gXDqowPnWX7c2cybHOpn0sGB4RowZha_XyIF-FzqvbkQtpaQ4XLx"
 
 
+discordWebhook = input("Enter your discord bots webhook: ")
+discord = Discord(url=discordWebhook)
 
 i = 0
 while i == 0:
-    current_time = datetime.datetime.now()
-    DATE = datetime.date.today().day
-    HOUR = datetime.datetime.now().hour   # the current hour
-    MINUTE = datetime.datetime.now().minute # the current minute
+    current_time = datetime.datetime.now()		#current everything (year-month-day-hour-minute)
+    MONTH = datetime.date.today().month		# current month
+    DAY = datetime.date.today().day		# current day
+    HOUR = datetime.datetime.now().hour   # current hour
+    MINUTE = datetime.datetime.now().minute #  current minute
     
     print(str(HOUR) + ":" + str(MINUTE))
     time.sleep(1)
-    if MINUTE == 0:
-        discord = Discord(url="https://discord.com/api/webhooks/1285532569252663329/Exwjdv428I8ev5W3gXDqowPnWX7c2cybHOpn0sGB4RowZha_XyIF-FzqvbkQtpaQ4XLx")
-        discord.post(content="Ärka yles")
+    
+    if MINUTE == 52:
+        discord.post(content="opa")
+
     
 
     
