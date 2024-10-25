2953ffd61be1e6f5e365d8870b357a3aa8a67f72
Ralfpuhk <162053779+Ralfpuhk@users.noreply.github.com>
2024-10-26 01:32:08 +0300

    Add files via upload

---
+++ moodulid_ja_test/weeklt_host_ver1.py
@@ -0,0 +1,162 @@
+from tkcalendar import Calendar
+import customtkinter as ctk
+import time
+import datetime
+import tkinter.scrolledtext as scrolledtext
+
+from spire.xls import *
+from spire.xls.common import *
+from spire.xls import Workbook
+
+import os
+import pandas as pd #pip install openpyxl
+import numpy as np
+
+import discordwebhook
+from discordwebhook import Discord
+
+import socket
+
+# ////////////////////////////////
+
+def digdin(message, nimi):
+    discordWebhook = "https://discord.com/api/webhooks/1285532569252663329/Exwjdv428I8ev5W3gXDqowPnWX7c2cybHOpn0sGB4RowZha_XyIF-FzqvbkQtpaQ4XLx"
+    discord = Discord(url=discordWebhook)#
+
+    discord.post(content = ("<@"+(str(nimi[:len(nimi)-5]))+">" + " has set the following notification to go off now: \n" + str(message)))
+
+
+def file_list_cck():
+    file_list = []
+    
+    for path in os.listdir(dir_name_slash):
+        if os.path.isfile(os.path.join(dir_name_slash, path)):           
+            file_list.append(os.path.basename(path).split('/')[-1])
+    return(file_list)
+
+def check():
+    
+
+    for file in file_list_cck():
+        df = pd.read_excel(os.path.join(dir_name_slash, file), header=None)  # Read without headers to get raw data
+        
+        values = df.values.flatten()
+        num_cols = 3
+        matrix = values.reshape(-1, num_cols).tolist()
+        #print(matrix)
+        for info in matrix:
+            
+            #lll = 1
+        
+            current_time = datetime.datetime.now()		#current everything (year-month-day-hour-minute)
+            month = datetime.date.today().month		# current month
+            day = datetime.date.today().day		# current day
+            hour = datetime.datetime.now().hour   # current hour
+            minute = datetime.datetime.now().minute #  current minute
+            year = datetime.datetime.now().year
+            second = datetime.datetime.now().second  
+
+            if info[0] == ((str(day) + "." + str(month) + "." + str(year))):
+                if info[1] == (str(hour) + ":" + str(minute)):
+                    #if lll == 1:
+                    digdin(info[2], file)
+                    print(info[2])
+    second = datetime.datetime.now().second                    #lll =+ 1
+    print("cycle done. Current second count is : " + str(second))
+    root.after(60000, check)
+
+def recieve_xslx(conn):
+
+    # Receive the length of the file name first (4 bytes)
+    byte_length = conn.recv(4)
+    byte_length_update = int.from_bytes(byte_length, 'big')  # Convert bytes to integer
+
+    flyname_bytes = conn.recv(byte_length_update)  # Read the specified length of bytes for the file name
+    flyname = flyname_bytes.decode('utf-8')  # Decode the filename
+    flypath = os.path.join(dir_name, flyname)  # Create the full file path in the 'info' dir_name
+    print(flypath)
+    # Open a file to write the received data
+    with open(flypath, 'wb') as f:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            f.write(data)
+
+    print("recieved")
+    time.sleep(0.5)# igaksjuhuks, et socket katki ei laheks kui funktsiooni liiga palju kasutada jarjest
+
+def connection():
+    s = socket.socket()
+    s.bind((IP, port))
+    s.settimeout(3)
+    try:
+        s.listen(1)
+        print("trying for connection...")
+        conn, addr = s.accept()
+        print("connected! :", addr)
+        
+        data = conn.recv(1024)
+#         print(data)
+        flyfile_name = data.decode()
+#         print(flyfile_name)
+        
+        
+        if flyfile_name in file_list_cck():
+            flyfile = os.path.join(dir_name_slash, flyfile_name)  
+            with open(flyfile, 'rb') as file:
+                while True:
+                    data = file.read(1024)
+                    if not data:
+                        break
+                conn.sendall(data) 
+            print("File sent successfully.")
+            time.sleep(0.5)
+            
+            recieve_xslx(conn) 
+            
+        elif flyfile_name not in file_list_cck():
+            conn.sendall("0".encode("utf-8"))
+            
+            recieve_xslx(conn)            
+                
+        else:
+            print("oopsy :), bokem")
+        conn.close()       
+    except:
+        print("no connection found")
+    
+    s.close()
+    root.after(1000, connection)
+    
+
+# ////////////////////////////////
+
+dir_name_slash = "info/"
+dir_name = "info"
+port = 54123
+IP = "192.168.1.142" # hosti IP
+
+# ///////////////////////////////
+
+try:
+    os.mkdir(dir_name)
+    print(f"Directory '{dir_name}' created successfully.")
+except FileExistsError:
+    print(f"Directory '{dir_name}' already exists.")
+except PermissionError:
+    print(f"Permission denied: Unable to create '{dir_name}'.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+    
+root = ctk.CTk()
+
+# ///////////////////////////////
+
+root.after(0, check)
+root.after(0, connection)
+
+# ///////////////////////////////
+
+root.mainloop()
+
