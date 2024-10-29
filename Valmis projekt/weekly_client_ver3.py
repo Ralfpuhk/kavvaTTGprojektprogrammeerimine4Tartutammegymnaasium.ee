e4c7a541c5d7d6f09e4aeeb40b1ed1e4fac74b6e
Ralfpuhk <162053779+Ralfpuhk@users.noreply.github.com>
2024-10-29 12:22:10 +0200

    Update weekly_client_ver3.py

--- Valmis projekt/weekly_client_ver3.py
+++ Valmis projekt/weekly_client_ver3.py
@@ -9,7 +9,7 @@
 # Programm saadab teate minuti jooksul, kuna oleneb programmi avamise ajast -> ei pruugi olla iga minuti alguses.
 # Kuidas saada oma discord ID: 
 # Vajuta discordis all vasakul nupule "User Settings" -> Vajuta "Advanced" -> Käivita "Developer Mode" -> Mine seadetest välja -> All vasakul vajuta oma discordi profiili peale -> Vajuta "Copy User ID"
-
+#NB! excel file ei tohi lahti olla
 port = 54123
 # IP = "" # hosti IP
 IP = "127.0.0.1" #muuda see ara, kui tahad valjaspool oma arvutit
