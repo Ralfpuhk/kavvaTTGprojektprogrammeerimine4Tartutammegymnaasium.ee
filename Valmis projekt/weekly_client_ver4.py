c0c50f1c77b8f7c1b3c706db9626b2451cf0af53
Ralfpuhk <162053779+Ralfpuhk@users.noreply.github.com>
2024-10-31 00:07:40 +0200

    Update weekly_client_ver4.py

--- Valmis projekt/weekly_client_ver4.py
+++ Valmis projekt/weekly_client_ver4.py
@@ -11,7 +11,8 @@
 # Vajuta discordis all vasakul nupule "User Settings" -> Vajuta "Advanced" -> Käivita "Developer Mode" -> Mine seadetest välja -> All vasakul vajuta oma discordi profiili peale -> Vajuta "Copy User ID"
 
 port = 54123
-IP = "127.0.0.1" # hosti IP. vajadusel muuda host programmi "IPv4" IP-ks. (tootab ainult local networkide peal kuna public server IP maksab raha)
+IP = "127.0.0.1" # hosti IP. vajadusel muuda host programmi "IPv4" IP-ks. Tee seda ka host programmis. 
+#(tootab ainult local networkide peal kuna public server IP maksab raha)
 
 import shutil
 
